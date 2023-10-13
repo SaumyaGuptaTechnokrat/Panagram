0857d7eb35bb894a55b3471918405018498811c7
SaumyaGuptaTechnokrat <65768914+SaumyaGuptaTechnokrat@users.noreply.github.com>
2023-10-13 19:04:08 +0530

    Changes

--- PangramChecker.java
+++ PangramChecker.java
@@ -1,38 +1,37 @@
-import java.util.HashSet;
 import java.util.Scanner;
-import java.util.Set;
-
 public class PangramChecker {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a sentence: ");
         String input = scanner.nextLine();
         boolean isPangram = pangramChecker(input.toLowerCase()); // Convert to lowercase for case-insensitivity
-        //The quick brown fox jumps over the lazy dog
-        //Input sentence example
+
         if (isPangram) {
             System.out.println("The input is a pangram.");
         } else {
             System.out.println("The input is not a pangram.");
         }
-        
+
         scanner.close();
     }
-    
+
     public static boolean pangramChecker(String input) {
-        Set<Character> setOfAlphabet = new HashSet<>();
-        
-        // Add all alphabets from 'a' to 'z' to the set
-        for (char ch = 'a'; ch <= 'z'; ch++) {
-            setOfAlphabet.add(ch);
-        }
-        
-        // Iterate through the input characters and remove them from the set
+        boolean[] alphabetPresent = new boolean[26]; // 26 letters in the English alphabet
+
+        // Iterate through the input characters and mark their presence
         for (char ch : input.toCharArray()) {
-            setOfAlphabet.remove(ch);
+            if (ch >= 'a' && ch <= 'z') {
+                alphabetPresent[ch - 'a'] = true;
+            }
         }
-        
-        // If the set is empty, it's a pangram
-        return setOfAlphabet.isEmpty();
+
+        // Check if all letters are present
+        for (boolean isPresent : alphabetPresent) {
+            if (!isPresent) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
